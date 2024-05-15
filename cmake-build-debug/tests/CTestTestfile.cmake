08485ce9796e904bf44dae4db9505d81e6662913
squirtleKK <krasnoperov2006@gmail.com>
2024-05-16 00:02:42 +0300

    last last

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/krasn/Desktop/OP/lab_11/labwork11-squirtleKKS/tests
-# Build directory: C:/Users/krasn/Desktop/OP/lab_11/labwork11-squirtleKKS/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/krasn/Desktop/OP/lab_11/labwork11-squirtleKKS/cmake-build-debug/tests/ttest[1]_include.cmake")
-subdirs("../_deps/googletest-build")
